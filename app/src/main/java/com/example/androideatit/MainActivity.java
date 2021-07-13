12e76bbfedcdf0cd234dbbb86139d377bd34cf51
Binhht7777 <Binhht7777@gmail.com>
2021-07-13 14:15:19 +0700

    Update source part9

--- app/src/main/java/com/example/androideatit/MainActivity.java
+++ app/src/main/java/com/example/androideatit/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         // ********** Anh xa cac control **********
         btnSignIn=(Button)findViewById(R.id.btnSignIn);
